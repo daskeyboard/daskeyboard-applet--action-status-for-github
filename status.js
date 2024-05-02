d6c94a55eceb6713012d80f3cd8a8012f69306e0
Soulaymane <157165107+SoulaymaneK@users.noreply.github.com>
2024-05-02 11:41:01 +0200

    Add files via upload

---
+++ status.js
@@ -0,0 +1,11 @@
+class Status {
+  static DEFAULT = '';
+  static PENDING = 'pending';
+  static SUCCESS = 'success';
+  static FAILURE = 'failure';
+  static ACTIVE = 'active';
+  static COMPLETED = 'completed';
+  static FAILED = 'failed';
+}
+
+module.exports = Status;
